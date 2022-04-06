acfd766f33a5bdfaa87e4059cb17a2c14a3a69d6
Konstantin Pochinkov <konstantin.pochinkov@solaredge.com>
2022-04-06 14:28:52 +0300

    Test actions

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Delete this (comment just to refresh action)
 ReactDOM.render(<App />, document.querySelector('#root'));
